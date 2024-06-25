ee0049ba1498d9c7d55dab6137b66c765fa8d0da
Anna Konstantynova <konstanna.dev@gmail.com>
2024-06-25 20:46:19 +0200

    Update routes

--- filter-app/src/app/app-routing.module.ts
+++ filter-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FinanzComponent } from './finanz/finanz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'finanz', component: FinanzComponent },
+  { path: '', redirectTo: '/finanz', pathMatch: 'full' }  // Redirect to finanz on root path
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
